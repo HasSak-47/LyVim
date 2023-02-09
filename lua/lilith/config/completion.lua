f2d821022a06034e502440b6d2efdd424baa23f4
lilith archmachine <lilith.alanis.255@gmail.com>
2023-02-09 10:27:41 -0600

    .

--- lua/lilith/config/completion.lua
+++ lua/lilith/config/completion.lua
@@ -104,8 +104,8 @@ cmp.setup {
     end,
   },
   sources = {
-    -- { name = "nvim_lsp" },
-    -- { name = "nvim_lua" },
+    { name = "nvim_lsp" },
+    { name = "nvim_lua" },
     { name = "luasnip" },
     { name = "buffer" },
     { name = "path" },
