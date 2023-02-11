223de0ec948a3a488b6b98774ea0b3cd40ff8a78
lilith archmachine <lilith.alanis.255@gmail.com>
2023-02-10 22:18:14 -0600

    started keymap rework

--- lua/lilith/keymaps.lua
+++ lua/lilith/keymaps.lua
@@ -1,56 +1,49 @@
-local opts = {noremap = true, silent = false}
-local keymap = vim.api.nvim_set_keymap
+local M = {
+	set_keymap = {
+		leader = ' ',
+		-- should change name later
+		window = {
+			resize = {
+				hor_inc = '<A-k>',
+				hor_dec = '<A-j>',
+				ver_inc = '<A-h>',
+				ver_dec = '<A-l>'
+			},
+			navigation = {
+				h = '<C-h>',
+				j = '<C-j>',
+				k = '<C-k>',
+				l = '<C-l>'
+			}
+		},
+		buffer = {
+			prev = '<S-h>',
+			next = '<S-h>'
+		}
+	},
+	buf_keymap = {},
+	insert_escape = 'jk',
 
--- [[ leader remapping ]]
-keymap("", "<Space>", "<Nop>", opts)
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
+	neovide = {
+		font = {
+			inc = 'nz+',
+			dec = 'nz-'
+		},
+		trans = {
+			inc = 'nz+',
+			dec = 'nz-'
+		}
+	},
 
--- [[ explorer ]]
-local ntre_toggle = ":NvimTreeToggle<CR>"
-local nlex_toggle = ":Lex 30<CR>"
-local toggle = nlex_toggle
+	theme = {
+		change = '<C-1>'
+	},
+	compleation = {
+		prev_item = '<A-k>',
+		next_item = '<A-j>',
 
-if enabled_plugins ~= nil then
-for _, v in pairs(enabled_plugins) do
-	-- this is the name of the nvim-tree config file
-	if v == 'completion' then
-		toggle = ntre_toggle
-	end
-end
-end
+		comfirm_selection = '<CR>'
+	}
+}
 
-keymap("n", "<leader>e", toggle, opts)
---keymap("n", "<leader>er", ":NvimTreeRefresh<CR>", opts)
-
--- [[ resize 'window' size ]]
-keymap("n", "<A-k>", ":resize -2<CR>", opts)
-keymap("n", "<A-j>", ":resize +2<CR>", opts)
-keymap("n", "<A-h>", ":vertical resize +2<CR>", opts)
-keymap("n", "<A-l>", ":vertical resize -2<CR>", opts)
-
-
--- [[ normal mode ]]
--- [[ navigation ]]
-	-- [[window navigation]]
-	-- it's like the navigation of i3
-keymap("n", "<C-h>", "<C-w>h", opts)
-keymap("n", "<C-j>", "<C-w>j", opts)
-keymap("n", "<C-k>", "<C-w>k", opts)
-keymap("n", "<C-l>", "<C-w>l", opts)
-
-	-- [[buffer navigation]]
-keymap("n", "<S-h>", ":bprevious<cr>", opts)
-keymap("n", "<S-l>", ":bnext<cr>", opts)
-
--- [[insert mode]]
-keymap("i", "jk", "<esc>", opts)
-keymap("n", "<leader>reaload", ":source $MYVIMRC<cr>", opts)
-
-if vim.fn.exists("g:neovide") then
-	vim.keymap.set("n", "nz+", function()  resize_gui_font(   1) end, opts)
-	vim.keymap.set("n", "nz-", function()  resize_gui_font(  -1) end, opts)
-	vim.keymap.set("n", "nt+", function() change_gui_trans( 0.1) end, opts)
-	vim.keymap.set("n", "nt-", function() change_gui_trans(-0.1) end, opts)
-end
-
+return M
