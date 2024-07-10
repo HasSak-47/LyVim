7acc526d5eb6b14f21afb27fdd9517d076cae602
arch machine <lilith.alanis.255@gmail.com>
2024-07-10 01:18:39 -0600

    react!

--- lua/ly/plugins/lsp_config/tsserver.lua
+++ lua/ly/plugins/lsp_config/tsserver.lua
@@ -1,3 +1,13 @@
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = {"javascript", "javascriptreact","typescript", "typescriptreact"},
+	callback = function ()
+		vim.opt.tabstop     = 2
+		vim.opt.shiftwidth  = 2
+		vim.opt.softtabstop = 2
+		vim.opt.expandtab   = false
+	end
+})
+
 local M = {
    init_options = {
    },
