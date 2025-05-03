b14ff72f373e0988fa3fad67a005a932c9754e3d
arch machine <lilith.alanis.255@gmail.com>
2025-05-02 22:03:34 -0600

    auto commit

--- lua/ly/opts.lua
+++ lua/ly/opts.lua
@@ -1,19 +1,21 @@
-vim.opt.clipboard   = "unnamedplus"
+vim.opt.clipboard = "unnamedplus"
 
-vim.opt.tabstop     = 4
-vim.opt.shiftwidth  = 4
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
 vim.opt.softtabstop = 4
-vim.opt.expandtab   = true
+vim.opt.expandtab = true
 
 vim.opt.breakindent = true
 
-vim.opt.number         = true
+vim.opt.number = true
 vim.opt.relativenumber = true
 
-vim.opt.hlsearch  = false
+vim.opt.hlsearch = false
 vim.opt.incsearch = false
 
 vim.opt.scrolloff = 8
 vim.opt.wrap = false
 
 vim.g.autoformat = true
+
+-- vim.lsp.set_log_level("trace")
