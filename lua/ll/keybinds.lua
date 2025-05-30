4f8441436a44072d9f6fa6e16fa1cbcca64ec942
Ulises Alanis <ulises.alanis.255@gmail.com>
2025-05-30 16:59:32 -0600

    added treesitter and began lsp + cmp

--- lua/ll/keybinds.lua
+++ lua/ll/keybinds.lua
@@ -1,3 +1,12 @@
-
+local opts = {remap = false}
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, opts)
+
+local builtin = require('telescope.builtin')
+
+vim.keymap.set("n", "<leader>pf", builtin.find_files, opts)
+vim.keymap.set("n", "<C-p>", builtin.git_files, opts)
+vim.keymap.set("n", "<leader>ps", function() 
+	builtin.grep_string({search = vim.fn.input("Grep > ")});
+end
+, opts)
